0a3b9357448ce0dc260b5f301dbdd9dd131b99de
Osama Awad <awado251@gamil.com>
2023-01-13 13:00:52 +0100

    Initial commit

---
+++ DB/127_0_0_1.sql
@@ -0,0 +1,56 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Gegenereerd op: 13 jan 2023 om 11:44
+-- Serverversie: 5.7.36
+-- PHP-versie: 7.4.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `php-pdo-crud-toets`
+--
+CREATE DATABASE IF NOT EXISTS `php-pdo-crud-toets` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `php-pdo-crud-toets`;
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `DureAuto`
+--
+
+DROP TABLE IF EXISTS `DureAuto`;
+CREATE TABLE IF NOT EXISTS `DureAuto` (
+  `Id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `Merk` varchar(200) NOT NULL,
+  `Model` varchar(200) NOT NULL,
+  `Topsnelheid` smallint(4) NOT NULL,
+  `Prijs` varchar(200) NOT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `DureAuto`
+--
+
+INSERT INTO `DureAuto` (`Id`, `Merk`, `Model`, `Topsnelheid`, `Prijs`) VALUES
+(1, 'Bugatti', 'La vioture', 420, '16500000'),
+(2, ' Rolls-ROYCE', 'Swaptail', 250, '10960000'),
+(3, 'Bugatti', 'EB110', 390, '7500000'),
+(4, 'Mercedes-Benz', 'Maybach Exelero', 350, '6700000'),
+(5, 'Koenigsegg', 'CCXR Trevita', 410, '4000000');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ toets.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>PHP PDO CRUD</h1>
+    
+    <form action="read.php" method="post">
+
+        <label for="Merk">Merk</label><br>
+        <input type="text" name="Merk" id="Merk"><br>
+        <br>
+        <label for="Model">Model:</label><br>
+        <input type="text" name="Model" id="Model"><br>
+        <br>
+        <label for="Topsnelhied">Topsnelhied:</label><br>
+        <input type="text" name="Topsnelhied" id="Topsnelhied"><br>
+        <br>
+        <label for="prijs">prijs:</label><br>
+        <input type="text" name="prijs" id="prijs"><br>
+        <br>
+        <input type="submit" value="Verstuur!">        
+
+    </form>
+
+</body>
+</html>
